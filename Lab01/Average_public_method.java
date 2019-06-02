a6ab435b599fa1443243497561428ca713ef3665
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-02 20:05:59 +1000

    Create Average_public_method.java

---
+++ Lab01/Average_public_method.java
@@ -0,0 +1,33 @@
+package example;
+
+/**
+ * Average example
+ * @author Aarthi
+ */
+public class Average {
+
+        /**
+         * Returns the average of an array of numbers
+         * @param the array of integer numbers
+         * @return the average of the numbers
+         */
+        public float computeAverage(int[] nums) {
+            float result = 0;
+            // Add your code
+            // nums is an 1d array contains only integers
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum = sum + nums[i];
+            }
+            // now sum contains the sum of all the integers
+            result = sum / (nums.length);
+            return result;
+        }
+
+        public static void main(String[] args) {
+            // Add your code
+        	Average result = new Average();
+            int[] nums = new int[] {1,2,3,4,5};
+            System.out.println("Average of the numbers is: " + result.computeAverage(nums));
+        }
+}
